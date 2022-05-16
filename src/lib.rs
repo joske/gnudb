b866fe0d2bc2d5f00a1d21cb08cbefda04c2f091
Jos Dehaes <jos.dehaes@gmail.com>
2022-05-16 23:16:48 +0200

    inline the structs

--- src/lib.rs
+++ src/lib.rs
@@ -8,9 +8,23 @@ use std::{
 
 use discid::DiscId;
 
-mod data;
+#[derive(Default, Debug)]
+pub struct Disc {
+    pub title: String,
+    pub artist: String,
+    pub year: Option<u16>,
+    pub genre: Option<String>,
+    pub tracks: Vec<Track>
+}
 
-use crate::data::{Disc, Track};
+#[derive(Default, Debug)]
+pub struct Track {
+    pub number: u32,
+    pub title: String,
+    pub artist: String,
+    pub duration: u64,
+    pub composer: Option<String>,
+}
 
 pub fn search_disc(discid: &DiscId) -> Result<Disc, String> {
     gnudb(discid)
