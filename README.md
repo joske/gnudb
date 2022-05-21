6befaa10af93cb170f7ee53236ced79dcf24a122
Jos Dehaes <jos.dehaes@gmail.com>
2022-05-21 22:33:25 +0200

    adapt example to async

--- README.md
+++ README.md
@@ -4,19 +4,21 @@ It uses the discid crate to query the discid from the CDROM/DVDROM drive.
 
 Right now only login, query and read are implemented, and only over CDDBP (not HTTP).
 
+The code is now fully async.
+
 Usage:
 
 ```Rust
 // get a disc id by querying the disc in the default CD/DVD ROM drive
 let discid = DiscId::read(Some(DiscId::default_device().as_str())).unwrap();
 // open a connection
-let mut con = gnudb::Connection::new().unwrap();
+let mut con = Connection::new().await.unwrap();
 // find a list of matches (could be multiple)
-let matches : Vec<Match> = con.query(&discid).unwrap();
+let matches: Vec<Match> = con.query(&discid).await.unwrap();
 // select the right match
-let m : Match = matches[2];
+let ref m: Match = matches[2];
 // read all the metadata
-let disc = con.read(&m).unwrap();
+let _disc = con.read(&m).await.unwrap();
 // close the connection (Drop trait is implemented, so not strictly necessary)
 con.close();
 ```
--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,11 @@
 //! Right now only login, query and read are implemented, and only over CDDBP (not HTTP)
 //! All I/O is now done async
 
-use std::{
-    net::Shutdown,
-};
 use async_std::{
+    io::{prelude::BufReadExt, BufReader, WriteExt},
     net::TcpStream,
-    io::{BufReader, prelude::BufReadExt, WriteExt},
 };
+use std::net::Shutdown;
 // use futures_io::if_std::AsyncWrite;
 
 use discid::DiscId;
@@ -301,6 +299,21 @@ fn parse_data(data: String) -> Disc {
         }
     }
     disc
+}
+
+async fn _example() {
+    // get a disc id by querying the disc in the default CD/DVD ROM drive
+    let discid = DiscId::read(Some(DiscId::default_device().as_str())).unwrap();
+    // open a connection
+    let mut con = Connection::new().await.unwrap();
+    // find a list of matches (could be multiple)
+    let matches: Vec<Match> = con.query(&discid).await.unwrap();
+    // select the right match
+    let ref m: Match = matches[2];
+    // read all the metadata
+    let _disc = con.read(&m).await.unwrap();
+    // close the connection (Drop trait is implemented, so not strictly necessary)
+    con.close();
 }
 
 #[cfg(test)]
