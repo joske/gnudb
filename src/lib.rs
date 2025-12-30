bf679fb70d8bf8cbbda95706e84768eaa2455cf1
Jos Dehaes <jos.dehaes@gmail.com>
2025-12-30 19:01:00 +0100

    move example code into doc comment

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,28 @@
 //!
 //! Right now only login, query and read are implemented, and only over CDDBP (not HTTP)
 //! All I/O is now done async using smol.
+//!
+//! Example usage:
+//! ```no_run
+//! use gnudb::{Connection, Match};
+//! use discid::DiscId;
+//! use smol::block_on;
+//!
+//! block_on(async {
+//!     // get a disc id by querying the disc in the default CD/DVD ROM drive
+//!     let discid = DiscId::read(Some(DiscId::default_device().as_str())).unwrap();
+//!     // open a connection
+//!     let mut con = Connection::new().await.unwrap();
+//!     // find a list of matches (could be multiple)
+//!     let matches: Vec<Match> = con.query(&discid).await.unwrap();
+//!     // select the right match
+//!     let m: &Match = &matches[2];
+//!     // read all the metadata
+//!     let _disc = con.read(m).await.unwrap();
+//!     // close the connection (Drop trait is implemented, so not strictly necessary)
+//!     con.close();
+//! });
+//! ```
 
 use log::debug;
 use smol::{io::BufReader, net::TcpStream, prelude::*};
@@ -384,21 +406,6 @@ fn parse_data(data: String) -> Result<Disc, GnuDbError> {
         }
     }
     Ok(disc)
-}
-
-async fn _example() {
-    // get a disc id by querying the disc in the default CD/DVD ROM drive
-    let discid = DiscId::read(Some(DiscId::default_device().as_str())).unwrap();
-    // open a connection
-    let mut con = Connection::new().await.unwrap();
-    // find a list of matches (could be multiple)
-    let matches: Vec<Match> = con.query(&discid).await.unwrap();
-    // select the right match
-    let m: &Match = &matches[2];
-    // read all the metadata
-    let _disc = con.read(m).await.unwrap();
-    // close the connection (Drop trait is implemented, so not strictly necessary)
-    con.close();
 }
 
 // run these tests with `--test-threads=1` or you'll get hangs because gnudb doesn't like multiple connections from same IP
